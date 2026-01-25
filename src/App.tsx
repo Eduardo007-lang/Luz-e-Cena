a476db61a8394b6047efd139e8d080d1c99fed24
eduardo2018ads@hotmail.com <eduardo2018ads@hotmail.com>
2026-01-25 13:28:12 -0300

    feat: configuration design token and created component banner

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import Banner from './components/Banner'
+
+function App() {
+
+  return (
+    <>
+      <Banner src='./banner_principal/banner.png' alt='banner principal'/>
+    </>
+  )
+}
+
+export default App
